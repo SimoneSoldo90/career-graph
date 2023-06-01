11c86dfc4e8ba179b973f62fbe7b9c5067072709
Antonio Zingariello <antonio.zingariello@bcsoft.com>
2023-06-01 15:07:01 +0200

    Creazione componente e modulo Generic-Table

---
+++ frontend/src/app/core/components/shared/generic-table/generic-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-table',
+  templateUrl: './generic-table.component.html',
+  styleUrls: ['./generic-table.component.css']
+})
+export class GenericTableComponent {
+
+}
