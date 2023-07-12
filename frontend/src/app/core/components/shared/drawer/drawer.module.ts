cc8c851c3d2f69e7898f797cb04ba365d9e927df
Simone Soldo <BCSoft@Simone-Soldo>
2023-07-12 12:32:45 +0200

    Aggiunta tasto test

--- frontend/src/app/core/components/shared/drawer/drawer.module.ts
+++ frontend/src/app/core/components/shared/drawer/drawer.module.ts
@@ -4,10 +4,11 @@ import { DrawerComponent } from './drawer.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { StatusManagerModule } from '../status-manager/status-manager.module';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [DrawerComponent],
-  imports: [CommonModule, MatSidenavModule, StatusManagerModule,MatIconModule],
+  imports: [CommonModule, MatSidenavModule, StatusManagerModule,MatIconModule,MatButtonModule],
   exports: [DrawerComponent]
 })
 export class DrawerModule { }
