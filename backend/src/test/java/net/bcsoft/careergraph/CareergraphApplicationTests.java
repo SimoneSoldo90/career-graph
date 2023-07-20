a14944acd37ab5d29a79b80ad43428f3187e4009
GabriTarchi <gabriele.tarchi@bcsoft.net>
2023-07-20 15:06:33 +0200

    aggiunta progetto backend

---
+++ backend/src/test/java/net/bcsoft/careergraph/CareergraphApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bcsoft.careergraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareergraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
