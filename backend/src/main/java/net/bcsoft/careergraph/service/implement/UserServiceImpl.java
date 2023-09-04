77b9616727e204c5ef770933daea144a3dbe3698
GabriTarchi <gabriele.tarchi@bcsoft.net>
2023-09-04 16:57:33 +0200

    in UserServiceImpl messo il @Service

--- backend/src/main/java/net/bcsoft/careergraph/service/implement/UserServiceImpl.java
+++ backend/src/main/java/net/bcsoft/careergraph/service/implement/UserServiceImpl.java
@@ -2,7 +2,9 @@ package net.bcsoft.careergraph.service.implement;
 
 import net.bcsoft.careergraph.dto.UserDTO;
 import net.bcsoft.careergraph.service.IUserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements IUserService {
     @Override
     public UserDTO getById(Integer id) {
