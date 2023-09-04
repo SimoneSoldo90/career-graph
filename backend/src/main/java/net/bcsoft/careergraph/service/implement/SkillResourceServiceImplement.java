575640bfbc92fab7f8f80729326dae3912f0ae23
Giackarcangioli <giacomo.arcangioli@bcsoft.net>
2023-09-04 16:06:02 +0200

    creati implementazioni di skillResources e skillService

---
+++ backend/src/main/java/net/bcsoft/careergraph/service/implement/SkillResourceServiceImplement.java
@@ -0,0 +1,40 @@
+package net.bcsoft.careergraph.service.implement;
+
+import net.bcsoft.careergraph.dto.ResourceDTO;
+import net.bcsoft.careergraph.service.ISkillResourceService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SkillResourceServiceImplement implements ISkillResourceService {
+    @Override
+    public ResourceDTO create(Integer skillId, ResourceDTO resourceDTO) {
+       System.out.println("Funziona!");
+       return null;
+    }
+
+    @Override
+    public List<ResourceDTO> getAll(Integer skillId) {
+        System.out.println("Funziona!");
+        return null;
+    }
+
+    @Override
+    public ResourceDTO getById(Integer skillId, Integer resourceId) {
+        System.out.println("Funziona!");
+        return null;
+    }
+
+    @Override
+    public ResourceDTO update(Integer skillId, Integer resourceId, ResourceDTO resourceDTO) {
+        System.out.println("Funziona!");
+        return null;
+    }
+
+    @Override
+    public ResourceDTO delete(Integer skillId, Integer resourceId) {
+        System.out.println("Funziona!");
+        return null;
+    }
+}
