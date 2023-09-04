187259921ba41f1d505b6f0349c396504bad4fc4
ginoAlpigiani <gino.alpigiani@bcsoft.net>
2023-09-04 16:17:07 +0200

    Service > Impl

---
+++ backend/src/main/java/net/bcsoft/careergraph/service/implement/StepImpl.java
@@ -0,0 +1,28 @@
+package net.bcsoft.careergraph.service.implement;
+
+import net.bcsoft.careergraph.dto.StepDTO;
+
+import java.util.List;
+
+public class StepImpl {
+    public StepDTO create(StepDTO stepDTO) {
+        System.out.println("Funziona!");
+        return null;
+    }
+    public List<StepDTO> getAll() {
+        System.out.println("Funziona!");
+        return null;
+    }
+    public StepDTO getById(Integer stepId) {
+        System.out.println("Funziona!");
+        return null;
+    }
+    public StepDTO update(Integer stepId, StepDTO stepDTO) {
+        System.out.println("Funziona!");
+        return null;
+    }
+    public StepDTO delete(Integer stepId) {
+        System.out.println("Funziona!");
+        return null;
+    }
+}
---
+++ backend/src/main/java/net/bcsoft/careergraph/service/implement/UserImpl.java
@@ -0,0 +1,10 @@
+package net.bcsoft.careergraph.service.implement;
+
+import net.bcsoft.careergraph.dto.UserDTO;
+
+public class UserImpl {
+    public UserDTO getById(Integer id) {
+        System.out.println("Funziona!");
+        return null;
+    }
+}
