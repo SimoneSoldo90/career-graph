12f54760c43d75cb62db2544d3520f5bf570e451
Paolo Tacconi <paolo.tacconi@bcsoft.net>
2023-09-04 16:08:29 +0200

    051 aggiunto application.yaml per mybatis

--- backend/src/main/java/net/bcsoft/careergraph/CareergraphApplication.java
+++ backend/src/main/java/net/bcsoft/careergraph/CareergraphApplication.java
@@ -1,9 +1,11 @@
 package net.bcsoft.careergraph;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = { "net.bcsoft.careergraph.entity.mapper" })
 public class CareergraphApplication {
 
 	public static void main(String[] args) {
