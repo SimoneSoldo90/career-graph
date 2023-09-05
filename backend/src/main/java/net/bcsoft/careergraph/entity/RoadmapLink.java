7f752f38260e1a2f9d0109bdbef1b359538a42b1
GabriTarchi <gabriele.tarchi@bcsoft.net>
2023-09-05 12:29:04 +0200

    create le entity Roadmap, RoadmapLink e ResourceType

---
+++ backend/src/main/java/net/bcsoft/careergraph/entity/RoadmapLink.java
@@ -0,0 +1,31 @@
+package net.bcsoft.careergraph.entity;
+
+public class RoadmapLink {
+    Long id;
+    Long idStep;
+    Long idRoadmap;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdStep() {
+        return idStep;
+    }
+
+    public void setIdStep(Long idStep) {
+        this.idStep = idStep;
+    }
+
+    public Long getIdRoadmap() {
+        return idRoadmap;
+    }
+
+    public void setIdRoadmap(Long idRoadmap) {
+        this.idRoadmap = idRoadmap;
+    }
+}
