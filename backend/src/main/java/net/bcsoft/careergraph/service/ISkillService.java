cfe89248223e79a43df9d44a5d80286fb44fd9bd
GabriTarchi <gabriele.tarchi@bcsoft.net>
2023-09-13 10:09:43 +0200

    aggiunti i throws delle eccezioni

--- backend/src/main/java/net/bcsoft/careergraph/service/ISkillService.java
+++ backend/src/main/java/net/bcsoft/careergraph/service/ISkillService.java
@@ -2,20 +2,23 @@ package net.bcsoft.careergraph.service;
 
 import net.bcsoft.careergraph.dto.ResourceDTO;
 import net.bcsoft.careergraph.dto.SkillDTO;
+import net.bcsoft.careergraph.exception.BadRequestException;
+import net.bcsoft.careergraph.exception.ConflictException;
+import net.bcsoft.careergraph.exception.NoContentException;
 import net.bcsoft.careergraph.exception.NotFoundException;
 
 import java.util.List;
 
 public interface ISkillService {
 
-    List<SkillDTO> findAll();
-    SkillDTO create(SkillDTO skillDTO);
-    SkillDTO update(SkillDTO skillDTO);
+    List<SkillDTO> findAll() throws NoContentException;
+    SkillDTO create(SkillDTO skillDTO) throws BadRequestException;
+    SkillDTO update(SkillDTO skillDTO) throws ConflictException;
     SkillDTO findById(Long skillId) throws NotFoundException;
-    List<SkillDTO> findByStepId(Long stepId);
-    List<ResourceDTO> findAllResource(Long skillId);
-    ResourceDTO createResource(Long skillId, ResourceDTO resourceDTO);
-    ResourceDTO findResourceById(Long skillId, Long resourceId);
-    ResourceDTO updateResource(Long skillId, Long resourceId, ResourceDTO resourceDTO);
+    List<SkillDTO> findByStepId(Long stepId) throws NotFoundException;
+    List<ResourceDTO> findAllResource(Long skillId) throws NoContentException;
+    ResourceDTO createResource(Long skillId, ResourceDTO resourceDTO)throws BadRequestException;
+    ResourceDTO findResourceById(Long skillId, Long resourceId) throws NotFoundException;
+    ResourceDTO updateResource(Long skillId, Long resourceId, ResourceDTO resourceDTO) throws ConflictException;
 
 }
--- backend/src/main/java/net/bcsoft/careergraph/service/implement/SkillServiceImpl.java
+++ backend/src/main/java/net/bcsoft/careergraph/service/implement/SkillServiceImpl.java
@@ -4,6 +4,8 @@ import net.bcsoft.careergraph.dto.ResourceDTO;
 import net.bcsoft.careergraph.dto.SkillDTO;
 import net.bcsoft.careergraph.entity.Resource;
 import net.bcsoft.careergraph.entity.Skill;
+import net.bcsoft.careergraph.exception.ConflictException;
+import net.bcsoft.careergraph.exception.NoContentException;
 import net.bcsoft.careergraph.exception.NotFoundException;
 import net.bcsoft.careergraph.exception.BadRequestException;
 import net.bcsoft.careergraph.mapper.ResourceMapper;
@@ -43,24 +45,21 @@ public class SkillServiceImpl implements ISkillService {
     }
 
     @Override
-    public List<SkillDTO> findAll() {
-        try{
-            List<Skill> skillList = skillMapper.findAll();
-            List<SkillDTO> skillDTOList = new ArrayList<>();
-            List<ResourceDTO> resourceDTOList = new ArrayList<>();
-            for(Skill skill : skillList){
-                List<Resource> resourceList = resourceMapper.findBySkillId(skill.getId());
-                for (Resource resource : resourceList){
-                    ResourceDTO resourceDTO = new ResourceDTO(resource.getId(), resource.getStepId(), resource.getSkillId(), resource.getResourceTypeId(), resource.getDescription(), resource.getUrl());
-                    resourceDTOList.add(resourceDTO);
-                }
-                SkillDTO skillDTO = new SkillDTO(skill.getId(), skill.getTitle(), skill.getDescription(), resourceDTOList);
-                skillDTOList.add(skillDTO);
+    public List<SkillDTO> findAll() throws NoContentException {
+        List<Skill> skillList = skillMapper.findAll();
+        List<SkillDTO> skillDTOList = new ArrayList<>();
+        List<ResourceDTO> resourceDTOList = new ArrayList<>();
+        for(Skill skill : skillList){
+            List<Resource> resourceList = resourceMapper.findBySkillId(skill.getId());
+            for (Resource resource : resourceList){
+                ResourceDTO resourceDTO = new ResourceDTO(resource.getId(), resource.getStepId(), resource.getSkillId(), resource.getResourceTypeId(), resource.getDescription(), resource.getUrl());
+                resourceDTOList.add(resourceDTO);
             }
-            return skillDTOList;
-        }catch(BadRequestException e){
-            throw e;
+            SkillDTO skillDTO = new SkillDTO(skill.getId(), skill.getTitle(), skill.getDescription(), resourceDTOList);
+            skillDTOList.add(skillDTO);
         }
+        return skillDTOList;
+
     }
 
     @Override
@@ -80,21 +79,17 @@ public class SkillServiceImpl implements ISkillService {
 
     @Override
     @Transactional
-    public SkillDTO create(SkillDTO skillDTO) {
-        try{
-            Skill skill = skillDTO.toEntity();
-            skillMapper.insert(skill);
-            Skill result = skillMapper.findById(skill.getId());
-            return new SkillDTO(result.getId(), result.getTitle(), result.getDescription(), null);
-        }catch (Exception e){
-            throw e;
-        }
+    public SkillDTO create(SkillDTO skillDTO) throws BadRequestException{
+        Skill skill = skillDTO.toEntity();
+        skillMapper.insert(skill);
+        Skill result = skillMapper.findById(skill.getId());
+        return new SkillDTO(result.getId(), result.getTitle(), result.getDescription(), null);
     }
 
 
     @Override
     @Transactional
-    public SkillDTO update(SkillDTO skillDTO) {
+    public SkillDTO update(SkillDTO skillDTO) throws ConflictException {
         try{
             Skill skill = skillDTO.toEntity();
             skillMapper.update(skill);
@@ -107,69 +102,49 @@ public class SkillServiceImpl implements ISkillService {
 
 
     @Override
-    public List<SkillDTO> findByStepId(Long stepId) {
-        try{
-            List<Skill> skillList = skillMapper.findByStepId(stepId);
-            List<SkillDTO> result = new ArrayList<>();
-            for (Skill skill : skillList) {
-                result.add(new SkillDTO(skill.getId(), skill.getTitle(), skill.getDescription(), null));
-            }
-            return result;
-        }catch (Exception e){
-            throw e;
+    public List<SkillDTO> findByStepId(Long stepId) throws NotFoundException{
+        List<Skill> skillList = skillMapper.findByStepId(stepId);
+        List<SkillDTO> result = new ArrayList<>();
+        for (Skill skill : skillList) {
+            result.add(new SkillDTO(skill.getId(), skill.getTitle(), skill.getDescription(), null));
         }
+        return result;
     }
 
     @Override
     public List<ResourceDTO> findAllResource(Long skillId) {
-        try{
-            List<Resource> resourceList = resourceMapper.findBySkillId(skillId);
-            List<ResourceDTO> resourceDTOList = new ArrayList<>();
-            for(Resource resource : resourceList){
-                ResourceDTO resourceDTO = new ResourceDTO(resource.getId(), resource.getSkillId(), resource.getStepId(),  resource.getResourceTypeId(), resource.getDescription(), resource.getUrl());
-                resourceDTOList.add(resourceDTO);
-            }
-            return resourceDTOList;
-        }catch (Exception e){
-            throw e;
+        List<Resource> resourceList = resourceMapper.findBySkillId(skillId);
+        List<ResourceDTO> resourceDTOList = new ArrayList<>();
+        for(Resource resource : resourceList){
+            ResourceDTO resourceDTO = new ResourceDTO(resource.getId(), resource.getSkillId(), resource.getStepId(),  resource.getResourceTypeId(), resource.getDescription(), resource.getUrl());
+            resourceDTOList.add(resourceDTO);
         }
+        return resourceDTOList;
     }
 
     @Override
     @Transactional
-    public ResourceDTO createResource(Long skillId, ResourceDTO resourceDTO) {
-        try {
-            Resource resource = resourceDTO.toEntity();
-            resourceMapper.insert(resource);
-            Resource result = resourceMapper.findById(resource.getId());
-            return new ResourceDTO(result.getId(), result.getStepId(), result.getSkillId(), result.getResourceTypeId(), result.getDescription(), result.getUrl());
-        }catch (Exception e){
-            throw e;
-        }
+    public ResourceDTO createResource(Long skillId, ResourceDTO resourceDTO) throws BadRequestException{
+        Resource resource = resourceDTO.toEntity();
+        resourceMapper.insert(resource);
+        Resource result = resourceMapper.findById(resource.getId());
+        return new ResourceDTO(result.getId(), result.getStepId(), result.getSkillId(), result.getResourceTypeId(), result.getDescription(), result.getUrl());
     }
 
 
     @Override
     public ResourceDTO findResourceById(Long skillId, Long resourceId) {
-        try{
-            Resource result = resourceMapper.findById(resourceId);
-            return new ResourceDTO(result.getId(), result.getStepId(), result.getSkillId(), result.getResourceTypeId(), result.getDescription(), result.getUrl());
-        }catch (Exception e){
-            throw e;
-        }
+        Resource result = resourceMapper.findById(resourceId);
+        return new ResourceDTO(result.getId(), result.getStepId(), result.getSkillId(), result.getResourceTypeId(), result.getDescription(), result.getUrl());
     }
 
 
     @Override
     @Transactional
-    public ResourceDTO updateResource(Long skillId, Long resourceId, ResourceDTO resourceDTO) {
-        try{
+    public ResourceDTO updateResource(Long skillId, Long resourceId, ResourceDTO resourceDTO) throws ConflictException{
             Resource resource = resourceDTO.toEntity();
             resourceMapper.update(resource);
             return new ResourceDTO(resource.getId(), resource.getSkillId(), resource.getStepId(), resource.getResourceTypeId(), resource.getDescription(), resource.getUrl());
-        } catch (Exception e){
-            throw e;
-        }
     }
 
 }
