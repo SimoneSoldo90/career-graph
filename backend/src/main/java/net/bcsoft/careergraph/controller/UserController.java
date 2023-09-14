6bde18389499deec1955e4163824f2bbc1a44a29
ginoAlpigiani <gino.alpigiani@bcsoft.net>
2023-09-14 17:02:01 +0200

    UserController: ultimate le eccezioni

--- backend/src/main/java/net/bcsoft/careergraph/controller/UserController.java
+++ backend/src/main/java/net/bcsoft/careergraph/controller/UserController.java
@@ -1,5 +1,7 @@
 package net.bcsoft.careergraph.controller;
 
+import net.bcsoft.careergraph.dto.ResourceDTO;
+import net.bcsoft.careergraph.dto.SkillDTO;
 import net.bcsoft.careergraph.dto.UserDTO;
 import net.bcsoft.careergraph.dto.UserSkillDTO;
 import net.bcsoft.careergraph.exception.BadRequestException;
@@ -7,9 +9,13 @@ import net.bcsoft.careergraph.exception.ConflictException;
 import net.bcsoft.careergraph.exception.NoContentException;
 import net.bcsoft.careergraph.exception.NotFoundException;
 import net.bcsoft.careergraph.service.IUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -22,48 +28,89 @@ public class UserController {
     }
 
     @GetMapping("/user/{userId}")
-    public UserDTO findUser(@PathVariable Long userId){
+    public ResponseEntity <UserDTO> findUser(@PathVariable Long userId){
+        UserDTO userDTO = null;
+        String sErrorMsg = "";
         try {
-            return userService.findById(userId);
+            userDTO = userService.findById(userId);
         } catch (NotFoundException e) {
-            throw new RuntimeException(e);
+            sErrorMsg = "Error getting message list: " + e.getMessage();
         }
+        ResponseEntity responseEntity = null;
+        if(userDTO != null){
+            responseEntity = ResponseEntity.ok(userDTO);
+        } else {
+            responseEntity = ResponseEntity.noContent().build();
+        }
+        return responseEntity;
     }
 
     @PostMapping("/users/{userId}/user-skills")
-    public UserSkillDTO createUserSkill(@PathVariable Long userId, @RequestBody UserSkillDTO userSkillDTO){
+    public ResponseEntity <UserSkillDTO> createUserSkill(@PathVariable Long userId, @RequestBody UserSkillDTO userSkillDTO){
+        UserSkillDTO userSkillDTO1 = null;
+        String sErrorMsg = "";
         try {
-            return userService.createUserSkill(userSkillDTO);
+            userSkillDTO1 = userService.createUserSkill(userSkillDTO);
         } catch (BadRequestException e) {
-            throw new RuntimeException(e);
+            sErrorMsg = "Error creating user skill: " + e.getMessage();
         }
+        ResponseEntity responseEntity = null;
+        if(userSkillDTO1 != null){
+            try{
+                responseEntity = ResponseEntity.created(new URI("/users/" + userId + "/user-skills/" + userSkillDTO1.id())).build();
+            }catch (URISyntaxException e){
+                responseEntity = ResponseEntity.internalServerError().body(e.getMessage());
+            }
+        }else{
+            responseEntity = ResponseEntity.badRequest().body(sErrorMsg);
+        }
+        return responseEntity;
     }
 
     @GetMapping("/users/{userId}/user-skills")
-    public List<UserSkillDTO> findUserSkillByUserId(@PathVariable Long userId){
-        try {
-            return userService.findUserSkillByUserId(userId);
-        } catch (NoContentException e) {
-            throw new RuntimeException(e);
+    public ResponseEntity <List<UserSkillDTO>> findUserSkillByUserId(@PathVariable Long userId){
+        List<UserSkillDTO> userSkillDTOList = null;
+        ResponseEntity responseEntity = null;
+        try{
+            userSkillDTOList = userService.findUserSkillByUserId(userId);
+            responseEntity = ResponseEntity.ok(userSkillDTOList);
+        }catch(NoContentException e){
+            responseEntity = ResponseEntity.notFound().build();
         }
+        return responseEntity;
     }
 
     @PutMapping("/users/{userId}/user-skills/{userSkillId}")
-    public UserSkillDTO updateUserSkill(@PathVariable Long userId, @PathVariable Long userSkillId, @RequestBody UserSkillDTO userSkillDTO){
-        try {
-            return userService.updateUserSkill(userSkillDTO);
-        } catch (ConflictException e) {
-            throw new RuntimeException(e);
+    public ResponseEntity <UserSkillDTO> updateUserSkill(@PathVariable Long userId, @PathVariable Long userSkillId, @RequestBody UserSkillDTO userSkillDTO){
+        UserSkillDTO userSkillDTO1 = null;
+        String sErrorMsg = "";
+        try{
+            userSkillDTO1 = userService.updateUserSkill(userSkillDTO);
+        }catch (ConflictException e){
+            sErrorMsg = "error updating skill : " + e.getMessage();
         }
+        ResponseEntity responseEntity = null;
+        if(userSkillDTO1 != null){
+            responseEntity = ResponseEntity.ok(userSkillDTO);
+        }
+        else{
+            responseEntity = ResponseEntity.status(HttpStatus.CONFLICT).body(sErrorMsg);
+        }
+        return responseEntity;
     }
 
     @GetMapping("/users/{userId}/user-skills/{userSkillId}")
-    public UserSkillDTO findUserSkill(@PathVariable Long userId, @PathVariable Long userSkillId){
-        try {
-            return userService.findUserSkillById(userSkillId);
-        } catch (NotFoundException e) {
-            throw new RuntimeException(e);
+    public ResponseEntity <UserSkillDTO> findUserSkillById(@PathVariable Long userId, @PathVariable Long userSkillId){
+        UserSkillDTO userSkillDTO = null;
+        ResponseEntity responseEntity = null;
+        try{
+            userSkillDTO = userService.findUserSkillById(userSkillId);
+            responseEntity = ResponseEntity.ok(userSkillDTO);
+        }catch(NotFoundException e){
+            responseEntity = ResponseEntity.notFound().build();
         }
+
+        return responseEntity;
     }
 
     /*@DeleteMapping("/users/{userId}/user-skills/{userSkillId}")
